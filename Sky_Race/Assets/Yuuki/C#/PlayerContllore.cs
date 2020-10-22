eb7ec4a3660a3a1787ab52cbe24b82eef72de3aa
Re-nia <yuki-kyuzato-19f@stu.kbc.ac.jp>
2020-10-22 15:33:45 +0900

    Bounceの多少の改良

---
+++ Sky_Race/Assets/Yuuki/C#/PlayerContllore.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerContllore : MonoBehaviour
+{
+    public float speed = 2.0f;
+    private Rigidbody rB;
+
+    void Start()
+    {
+        rB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        rB.AddForce(x * speed, 0, z * speed, ForceMode.Impulse);
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Buns")
+        {
+            StartCoroutine("WaitKeyInput");
+        }
+    }
+    IEnumerator WaitKeyInput()
+    {
+        this.gameObject.GetComponent<PlayerContllore>().enabled = false;
+        {
+            yield return new WaitForSeconds(1.0f);
+        }
+        this.gameObject.GetComponent<PlayerContllore>().enabled = true;
+    }
+}
