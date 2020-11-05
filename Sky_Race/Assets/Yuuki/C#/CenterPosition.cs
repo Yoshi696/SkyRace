080feca8d235637b2f2b6d7b30ba051ad58e607b
Re-nia <yuki-kyuzato-19f@stu.kbc.ac.jp>
2020-11-05 14:34:38 +0900

    反射のスクリプト＆プレイヤーのスクリプトを多少変更しました

---
+++ Sky_Race/Assets/Yuuki/C#/CenterPosition.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CenterPosition : MonoBehaviour
+{
+
+    void FixedUpdate()
+    {
+        GameObject playerObj = GameObject.Find("Player");
+        if (playerObj != null)
+        {
+            transform.position = playerObj.transform.position;
+        }
+
+        if (playerObj == null)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
