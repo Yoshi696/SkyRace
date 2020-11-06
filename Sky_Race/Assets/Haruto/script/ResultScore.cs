f77a2fd7d9356f767b4c4672f69b260eff988dae
shiromaharuto <haruto-shiroma-19f@stu.kbc.ac.jp>
2020-11-06 09:58:03 +0900

    俺の遺志を継いでくれ・・・さらばだ・・・

---
+++ Sky_Race/Assets/Haruto/script/ResultScore.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResultScore : MonoBehaviour
+{
+    private Text GoalScore;
+    private Text distanceScore;
+    private Text TotalScore;
+    private int GoalPoint;
+
+    void Start()
+    {
+        GoalScore = GameObject.Find("goalpoint").GetComponent<Text>();
+        distanceScore = GameObject.Find("kyori").GetComponent<Text>();
+        TotalScore = GameObject.Find("totalscore").GetComponent<Text>();
+        GoalPoint = 0;
+        SetScoreText(GoalPoint);
+    }
+
+        private void SetScoreText(int Score)
+        {
+            GoalScore.text = "GoalPoint:" + Score.ToString();
+        }
+
+    public void AddScore(int point)
+    {
+        GoalPoint += point;
+        SetScoreText(GoalPoint);
+    }
+
+}
