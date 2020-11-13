1d20ffe56e2823f3e7810c1aa6450ed69b68d7fe
Re-nia <yuki-kyuzato-19f@stu.kbc.ac.jp>
2020-11-13 11:51:13 +0900

    横風更新(後ほど調整予定)

--- Sky_Race/Assets/Yuuki/C#/Crosswind.cs
+++ Sky_Race/Assets/Yuuki/C#/Crosswind.cs
@@ -7,16 +7,27 @@ public class Crosswind : MonoBehaviour
     public float ClossMove = 0.25f;
     private float clossmove=0.1f;
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Vector3 myobj = this.transform.position;
+            Vector3 targ = GameObject.Find("Player").transform.position;
+            if (myobj.x > targ.x)
+            {
+                clossmove = -ClossMove;
+            }
+            else if (myobj.x <= targ.x)
+            {
+                clossmove = ClossMove;
+            }
+        }
+    }
     void OnTriggerStay()
     {
         Vector3 myobj = this.transform.position;
         Vector3 targ = GameObject.Find("Player").transform.position;
-        if (myobj.x > targ.x) {
-            clossmove = -ClossMove;
-        }else if (myobj.x < targ.x)
-        {
-            clossmove = ClossMove;
-        }
+        
         GameObject.Find("Player").transform.position = new Vector3(targ.x + clossmove, targ.y, targ.z);
     }
 }
