7be105affa45f159db4767f4619448637f138d9d
shiromaharuto <haruto-shiroma-19f@stu.kbc.ac.jp>
2020-11-13 12:44:06 +0900

    ゲームオーバーと色々

---
+++ Sky_Race/Assets/Haruto/script/GameOver.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    private GameObject mainCamera;
+    private GameObject subCamera;
+
+    public Text OverText;
+
+    void Start()
+    {
+        mainCamera = GameObject.Find("Camera");
+        subCamera = GameObject.Find("SubCamera");
+        OverText.enabled = false;
+
+        subCamera.SetActive(false);
+        mainCamera.SetActive(true);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Over")
+        {
+            mainCamera.SetActive(false);
+            subCamera.SetActive(true);
+            OverText.enabled = true;
+            Invoke("LoadScene", 2f);
+        }
+    }
+
+    void LoadScene()
+    {
+        SceneManager.LoadScene("GameResult");
+    }
+}
