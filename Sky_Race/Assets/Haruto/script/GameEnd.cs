62b396d388e773b61c5f3934b370544bdf415ec0
shiromaharuto <haruto-shiroma-19f@stu.kbc.ac.jp>
2020-11-16 08:56:26 +0900

    遅くなって済まない・・・　存分に取ってくれ

---
+++ Sky_Race/Assets/Haruto/script/GameEnd.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEnd : MonoBehaviour
+{
+	//　ゲーム終了ボタンを押したら実行する
+	public void EndGame()
+	{
+	#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+	#elif UNITY_WEBPLAYER
+		Application.OpenURL("http://www.yahoo.co.jp/");
+	#else
+		Application.Quit();
+	#endif
+	}
+}
