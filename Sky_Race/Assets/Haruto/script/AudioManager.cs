763dab994353bf1ab08f1a764ab136ffecb34e3a
shiromaharuto <haruto-shiroma-19f@stu.kbc.ac.jp>
2020-11-18 16:05:52 +0900

    効果音の追加（飛び立つ、スピードアップ等）、Zキーでボタンを押せるようにしました。あと背景の調整

--- Sky_Race/Assets/Haruto/script/AudioManager.cs
+++ Sky_Race/Assets/Haruto/script/AudioManager.cs
@@ -7,6 +7,8 @@ public class AudioManager : MonoBehaviour
     private AudioSource audioSource;
 
     public AudioClip sound01;
+    public AudioClip sound02;
+    public AudioClip sound03;
 
     void Start()
     {
@@ -18,6 +20,14 @@ public class AudioManager : MonoBehaviour
         if (other.gameObject.tag == "Jump")
         {
             audioSource.PlayOneShot(sound01);
+        }
+        if(other.gameObject.tag == "Take of")
+        {
+            audioSource.PlayOneShot(sound02);
+        }
+        if(other.gameObject.tag == "Turbo")
+        {
+            audioSource.PlayOneShot(sound03);
         }
     }
 }
