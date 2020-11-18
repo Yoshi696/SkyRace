15996d7f827e9efc02b522b3490539fe1d672290
shiromaharuto <haruto-shiroma-19f@stu.kbc.ac.jp>
2020-11-18 09:35:58 +0900

    忘れてた

---
+++ Sky_Race/Assets/Haruto/script/AudioManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    public AudioClip sound01;
+
+    void Start()
+    {
+        audioSource = gameObject.AddComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Jump")
+        {
+            audioSource.PlayOneShot(sound01);
+        }
+    }
+}
