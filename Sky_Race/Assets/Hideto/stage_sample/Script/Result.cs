00677944a58b5c7061d30da0f4ac0cb6b5ff9669
hidetoo <hideto-oshiro-19h@stu.kbc.ac.jp>
2020-11-19 11:13:09 +0900

    プレイヤーの飛行距離の計測更新

--- Sky_Race/Assets/Hideto/stage_sample/Script/Result.cs
+++ Sky_Race/Assets/Hideto/stage_sample/Script/Result.cs
@@ -5,16 +5,18 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 public class Result : MonoBehaviour
 {
-    //距離を表示しないならここから
+    //数値の表示用↓
     [SerializeField]
-    private Transform targetObj;
+    private Transform targetObj = null;
     [SerializeField]
-    private Text PPUI;
+    private Text PPUI = null;
     [SerializeField]
-    private Text SPUI;
+    private Text SPUI = null;
     [SerializeField]
-    private Text DSUI;
-    //ここまではコメントアウトする
+    private Text SUMUI = null;
+    [SerializeField]
+    private Text PDUI = null;
+    //数値の表示用↑
 
     private float colliderOffset;
 
@@ -25,9 +27,8 @@ public class Result : MonoBehaviour
     private float pm = 100;
     private int GoalPoint;
     private double distance;
-
-    //デバック用の変数
-    public int Log = 2;//デバック表示 1 非表示 1以外
+    private Vector3 v2;
+    private double sum = 0.0f;
 
     //Goalの文字テキスト
     public Text GoalText;
@@ -37,50 +38,49 @@ public class Result : MonoBehaviour
         colliderOffset = GetComponent<CharacterController>().radius + targetObj.GetComponent<CharacterController>().radius;
         GoalText.enabled = false;
     }
+    private void OnEnable()
+    {
+        v2 = targetObj.transform.position; //最初のプレイヤーの位置を保存
+
+    }
     void Update()
     {
         //スタート座標とプレイヤー座標の取得
         Vector3 v = transform.position;//プレイヤーの座標
-        Vector3 v2 = targetObj.position;//スタート座標
-        distance = (v.z - v2.z) - colliderOffset;//距離計算
 
+        if (Input.anyKeyDown )//何らかのキー又はマウスが押されたとき
+        {
+            sum += distance;//飛行距離を合計する
+            v2 = transform.position;//キーが押されたときの座標取得
+
+        }
+        distance = Vector3.Distance(v,v2) - colliderOffset;// 今の距離とその前の距離の計算キーが押されたら更新する
+
+        //  distance = (v.z - v2.z) - colliderOffset;//Z距離計算
+        Debug.Log(sum.ToString("0.00m"));
+        //数値の表示
         if (PPUI != null)
         {
             //プレイヤーのz座標を表示
-            PPUI.text = v.z.ToString("0.00m");
+            PPUI.text = "プレイヤー座標"+v.z.ToString("0.00m");
             //スタートのz座標を表示
-            SPUI.text = v2.z.ToString("0.00m");
-            //プレイヤーとスタートのz軸の距離を表示(スコアに使うのはこっちかな)
-            DSUI.text = distance.ToString("0.00m");
+            SPUI.text = "スタート座標"+v2.z.ToString("0.00m");
+            //プレイヤーの飛行距離を表示(スコアに使うのはこっちかな)
+            SUMUI.text = "距離合計"+ sum.ToString("0.00m");
+
+            PDUI.text = "座標:" + distance.ToString("0.00m");
 
         }
-        else if(Log == 1)
-        {//上の条件に当てはまらない時のデバック表示
-            Debug.Log(v.z.ToString("0.00m"));
-            Debug.Log(v2.z.ToString("0.00m"));
-            Debug.Log(distance.ToString("0.00m"));
-        }
+
+        ////デバック表示
+        //    Debug.Log(FastPos.ToString("0.00m"));
+        //    Debug.Log(v.z.ToString("0.00m"));
+        //    Debug.Log(v2.z.ToString("0.00m"));
+        //    Debug.Log(distance.ToString("0.00m"));
+        //
 
     }
 
-    public void onClick()
-    {
-
-        // シーン切り替え
-//        SceneManager.LoadScene("GameResult");
-    }
-    private void GameResultLoaded(Scene next, LoadSceneMode mode)
-    {
-        // シーン切り替え後のスクリプトを取得
-        var gameManager = GameObject.FindWithTag("ResultScore").GetComponent<ResultScore>();
-
-        // データを渡す処理
-        gameManager.GoalPoint = GoalPoint;
-        gameManager.Distance = distance;
-
-        // イベントから削除
-        SceneManager.sceneLoaded -= GameResultLoaded;
-    }
 
     private void OnTriggerEnter(Collider other)    //ゴールに接触した瞬間に入る
     {
@@ -103,9 +103,21 @@ public class Result : MonoBehaviour
 
         }
         Debug.Log(GoalPoint);
+        //ゲームオーバーになった時の処理
+        if(other.gameObject.tag == "Over")
+        {
+            sum += distance;
+            v2 = transform.position;//プレイヤー座標
+
+            // イベントに登録
+            SceneManager.sceneLoaded += GameResultLoaded;
+            Invoke("LoadScene", 1f);
+
+        }
+
     }
     private void OnTriggerExit(Collider other)
-    {
+    {//トリガーから出たとき
         if (other.gameObject.tag == "High_P")
         {
             Debug.Log("出たから中得点");
@@ -117,6 +129,11 @@ public class Result : MonoBehaviour
             Debug.Log("出たから低得点");
             GoalPoint = Low;
 
+        }
+        if (other.gameObject.tag == "JosouJ")
+        {
+            sum = 0;//歩行距離合計の初期化
+            v2 = targetObj.transform.position; //最初のプレイヤーの位置を保存
         }
     }
     private void OnCollisionStay(Collision other)
@@ -130,6 +147,9 @@ public class Result : MonoBehaviour
             }
             else if(pm <= 0)
             {
+                sum += distance;
+                v2 = transform.position;//プレイヤー座標
+
                 GoalText.enabled = true;
                 //              GameObject gm = GameObject.Find("ResultScore");
                 //            gm.GetComponent<ResultScore>().AddScore(GoalPoint);
@@ -144,5 +164,17 @@ public class Result : MonoBehaviour
     {
         //Result画面を読み込む
         SceneManager.LoadScene("GameResult");
+    }
+    private void GameResultLoaded(Scene next, LoadSceneMode mode)
+    {
+        // シーン切り替え後のスクリプトを取得
+        var gameManager = GameObject.FindWithTag("ResultScore").GetComponent<ResultScore>();
+
+        // データを渡す処理
+        gameManager.GoalPoint = GoalPoint;
+        gameManager.Distance = sum;
+
+        // イベントから削除
+        SceneManager.sceneLoaded -= GameResultLoaded;
     }
 }
