f413379ee167b0aba91fc1d2b9283804636c6494
shiromaharuto <haruto-shiroma-19f@stu.kbc.ac.jp>
2020-11-24 10:47:25 +0900

    スタートの操作をわかりやすくしたと思う・・・

---
+++ Sky_Race/Assets/Haruto/script/Manual.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Manual : MonoBehaviour
+{
+    public Text ManualText;
+    public Image backScreen;
+    public Image Bbutton;
+
+    void Start()
+    {
+        ManualText.enabled = true;
+        backScreen.enabled = true;
+        Bbutton.enabled = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "move")
+        {
+            Bbutton.enabled = false;
+        }
+
+        if (other.gameObject.tag == "Take of")
+        {
+            ManualText.enabled = false;
+            backScreen.enabled = false;
+        }
+    }
+}
---
+++ Sky_Race/Assets/Haruto/script/Manual1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Manual1 : MonoBehaviour
+{
+    public Image buttonPush;
+
+    void Start()
+    {
+        buttonPush.enabled = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "move")
+        {
+            buttonPush.enabled = true;
+        }
+
+        if(other.gameObject.tag == "Take of")
+        {
+            buttonPush.enabled = false;
+        }
+    }
+}
