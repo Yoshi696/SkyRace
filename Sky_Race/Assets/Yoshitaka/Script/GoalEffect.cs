0638ccea4c42a24f4c316b89b89af9764ca12b3e
Yoshi696 <qiandaijicheng@gmail.com>
2020-11-25 17:29:32 +0900

    花火に関して（未完成）

---
+++ Sky_Race/Assets/Yoshitaka/Script/GoalEffect.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalEffect : MonoBehaviour
+{
+    //花火のエフェクト（赤の場合）
+    private ParticleSystem _Hanabi1;
+    //private ParticleSystem Hanabi2;
+    //private ParticleSystem Hanabi3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _Hanabi1 = GameObject.Find("Hanabi1").GetComponent<ParticleSystem>();
+        //Hanabi2 = GameObject.Find("Hanabi2").GetComponent<ParticleSystem>();
+        //Hanabi3 = GameObject.Find("Hanabi3").GetComponent<ParticleSystem>();
+        _Hanabi1.Stop();
+        //Hanabi2.Stop();
+        //Hanabi3.Stop();
+    }
+
+    private void Update()
+    {
+        //Hanabi1.Play();
+        //Hanabi2.Play();
+        //Hanabi3.Play();
+    }
+
+    //private void OnTriggerEnter(Collider other)    //ゴールに接触した瞬間に入る
+    //{
+    //    if (other.gameObject.tag == "High_P")
+    //    {
+    //        Hanabi1.Play();
+    //        //Hanabi2.Play();
+    //        //Hanabi3.Play();
+
+    //    }
+    //    else if (other.gameObject.tag == "Medium_P")
+    //    {
+    //        Hanabi1.Play();
+    //        //Hanabi2.Play();
+    //        //Hanabi3.Play();
+
+    //    }
+    //    else if (other.gameObject.tag == "Low_P")
+    //    {
+    //        Hanabi1.Play();
+    //        //Hanabi2.Play();
+    //        //Hanabi3.Play();
+
+    //    }
+    //}
+}
