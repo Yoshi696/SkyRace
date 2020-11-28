f7367b702707b6f08d1fbf21fc6977e8f1a975bf
shiromaharuto <haruto-shiroma-19f@stu.kbc.ac.jp>
2020-11-28 22:09:14 +0900

    環境音追加

--- Sky_Race/Assets/Haruto/script/AudioManager.cs
+++ Sky_Race/Assets/Haruto/script/AudioManager.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour
 {
     private AudioSource audioSource;
+    private AudioSource audioSource2;
 
     public AudioClip sound01;
     public AudioClip sound02;
     public AudioClip sound03;
+    public AudioClip sound04;
 
     void Start()
     {
         audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource2 = gameObject.AddComponent<AudioSource>();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -28,6 +31,10 @@ public class AudioManager : MonoBehaviour
         if(other.gameObject.tag == "Turbo")
         {
             audioSource.PlayOneShot(sound03);
+        }
+        if(other.gameObject.tag == "Take of")
+        {
+            audioSource2.PlayOneShot(sound04);
         }
     }
 }
