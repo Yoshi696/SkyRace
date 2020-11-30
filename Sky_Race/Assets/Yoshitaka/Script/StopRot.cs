54502761263923a769a2f67d10a025fddbc816e9
Yoshi696 <qiandaijicheng@gmail.com>
2020-11-30 15:40:45 +0900

    加速竜巻、すこし変更しました

---
+++ Sky_Race/Assets/Yoshitaka/Script/StopRot.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopRot : MonoBehaviour
+{
+	public GameObject objTarget;
+	public Vector3 offset;
+
+	void Start()
+	{
+		updatePostion();
+	}
+
+	void LateUpdate()
+	{
+		updatePostion();
+	}
+
+	void updatePostion()
+	{
+		Vector3 pos = objTarget.transform.localPosition;
+
+		transform.localPosition = pos + offset;
+	}
+
+}
