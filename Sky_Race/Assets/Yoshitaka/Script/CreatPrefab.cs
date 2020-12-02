13cbbfccde31bff365fbbb1ffdc4dcac0a9532e0
Yoshi696 <qiandaijicheng@gmail.com>
2020-12-02 09:49:19 +0900

    修正+プレイヤーの見た目追加

--- Sky_Race/Assets/Yoshitaka/Script/CreatPrefab.cs
+++ Sky_Race/Assets/Yoshitaka/Script/CreatPrefab.cs
@@ -7,7 +7,7 @@ public class CreatPrefab : MonoBehaviour
     public GameObject cloud;
 
     private int CreatTime;
-    private int intvalTime = 300;
+    private int intvalTime = 500;
 
     // Update is called once per frame
     void Update()
@@ -31,7 +31,7 @@ public class CreatPrefab : MonoBehaviour
         //cloud.transform.position = new Vector3(dir.x, dir.y, dir.z + 100);
         Vector3 dir = newCloud.transform.forward;
 
-        newCloud.GetComponent<Rigidbody>().AddForce(dir * -30f, ForceMode.Impulse);
+        newCloud.GetComponent<Rigidbody>().AddForce(dir * -10f, ForceMode.Impulse);
 
         newCloud.name = cloud.name;
         Destroy(newCloud, 20.0f);
