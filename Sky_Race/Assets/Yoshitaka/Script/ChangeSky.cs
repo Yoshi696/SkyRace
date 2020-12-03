88797900863751033a9f4bb7abd0c3951f50e974
Yoshi696 <qiandaijicheng@gmail.com>
2020-12-03 16:29:30 +0900

    セレクトを少し調整

--- Sky_Race/Assets/Yoshitaka/Script/ChangeSky.cs
+++ Sky_Race/Assets/Yoshitaka/Script/ChangeSky.cs
@@ -5,18 +5,38 @@ using UnityEngine;
 public class ChangeSky : MonoBehaviour
 {
     // Skyboxのマテリアル
+    public Material sky0;
     public Material sky1;
+
+    //変数取得
+    //public SelectStageg sky;
+    private int skyselect;
 
     // Start is called before the first frame update
     void Start()
     {
-
+       // skyselect = sky.GetChangeSky();
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKey(KeyCode.C))
+        {
+            skyselect = 1;
+        }
+        if (Input.GetKey(KeyCode.V))
+        {
+            skyselect = 0;
+        }
+
+        if (skyselect == 0)
+        {
+            // Skyboxを変更する
+            RenderSettings.skybox = sky0;
+        }
+
+        if (skyselect == 1)
         {
             // Skyboxを変更する
             RenderSettings.skybox = sky1;
---
+++ Sky_Race/Assets/Yoshitaka/Script/SelectStageg.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectStageg : MonoBehaviour
+{
+    private float befor_button;
+    public Text text;
+
+    bool clickone = false;
+    bool clicktwo = false;
+
+    private int no = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float sayu = Input.GetAxisRaw("Horizontal");
+
+        if (clickone == true && no != 3)
+        {
+            no++;
+            clickone = false;
+            clicktwo = false;
+        }
+        if (clicktwo == true && no != 0)
+        {
+            no--;
+            clickone = false;
+            clicktwo = false;
+        }
+
+        if (no == 2)
+        {
+            text.text = "夜";
+        }
+        if (no == 1)
+        {
+            text.text = "昼";
+        }
+        if (no == 0)
+        {
+            text.text = "朝";
+        }
+
+        //if (clickone == true)
+        //{
+        //    clickone = false;
+        //}
+        //if (clicktwo == true)
+        //{
+        //    clicktwo = false;
+        //}
+
+        if(sayu == 1 && befor_button == 0.0f) 
+        {
+            clickone = true;
+        }
+        if (sayu == -1 && befor_button == 0.0f)
+        {
+            clicktwo = true;
+        }
+
+        befor_button = sayu;
+    }
+
+    public int GetChangeSky()
+    {
+        return no;
+    }
+}
+
