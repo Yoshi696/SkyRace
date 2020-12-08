049e9e38425b6a46136fcae75cc321d99d9f84b0
shiromaharuto <haruto-shiroma-19f@stu.kbc.ac.jp>
2020-12-08 16:09:10 +0900

    上げ忘れたで候ンンンンン

---
+++ Sky_Race/Assets/Haruto/script/Menu2.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu2 : MonoBehaviour
+{
+	Button next;
+
+    void Start()
+	{
+		// ボタンコンポーネントの取得
+		next = GameObject.Find("/Canvas/next").GetComponent<Button>();
+		next.Select();
+	}
+}
