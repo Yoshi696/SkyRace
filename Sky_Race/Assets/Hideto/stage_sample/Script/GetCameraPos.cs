31314507e2f3e0280e35ac9113f1ff3a47a5ed35
hidetoo <hideto-oshiro-19h@stu.kbc.ac.jp>
2020-12-10 21:01:05 +0900

    ゴールについた時のカメラの調整

---
+++ Sky_Race/Assets/Hideto/stage_sample/Script/GetCameraPos.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetCameraPos : MonoBehaviour
+{
+
+
+    GameObject playerObj;
+    private Vector3 a;
+    private void FixedUpdate()
+    {
+        playerObj = GameObject.Find("Player");
+        a = playerObj.transform.position;
+    }
+    private void Update()
+    {
+        transform.position = new Vector3(a.x, a.y, a.z);
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+    }
+
+
+}
