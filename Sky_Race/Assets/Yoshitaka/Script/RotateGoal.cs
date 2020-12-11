adfdf19cbec4dc10bb4ca2f0fa12319f760e3d39
Yoshi696 <qiandaijicheng@gmail.com>
2020-12-11 11:19:53 +0900

    スタート地点回転させてみた

---
+++ Sky_Race/Assets/Yoshitaka/Script/RotateGoal.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateGoal : MonoBehaviour
+{
+    bool key;
+    // Start is called before the first frame update
+    void Start()
+    {
+        key = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (key == true)
+        {
+            if (transform.localEulerAngles.z <= 180 /*&& transform.localEulerAngles.z >= 170*/)
+            {
+                //回転させる角度
+                float angle = -1;
+
+                //カメラを回転させる
+                transform.RotateAround(transform.position, Vector3.back, angle);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            key = true;
+        }
+    }
+}
