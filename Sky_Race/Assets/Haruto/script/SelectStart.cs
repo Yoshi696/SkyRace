4d113d34ed905fcfb1aee0778d87d57a84f3e232
Yoshi696 <qiandaijicheng@gmail.com>
2020-12-12 13:32:38 +0900

    リザルトの音を足した。あとMakeDebugに消し忘れがあったので対応しました

--- Sky_Race/Assets/Haruto/script/SelectStart.cs
+++ Sky_Race/Assets/Haruto/script/SelectStart.cs
@@ -9,8 +9,18 @@ public class SelectStart : MonoBehaviour
     Selectable Retry;　//リトライボタン
     Selectable Title;   //タイトルボタン
 
+    private AudioSource audioSource;
+
+    public AudioClip sound01;
+    public AudioClip sound02;
+
+    private float befor_button;
+
+
     void Start()
     {
+        audioSource = gameObject.AddComponent<AudioSource>();
+
         //マウスポインタ消すやつ
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.Locked;
@@ -23,16 +33,26 @@ public class SelectStart : MonoBehaviour
 
     void Update()
     {
+        float click = Input.GetAxisRaw("Horizontal");
+
         //クリックされた時 かつ lockStateがLockedではない時だけ実行
         if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1) || Input.GetMouseButtonDown(2) && Cursor.lockState == CursorLockMode.Locked)
         {
             OnClick();  //クリックされた時の処理
         }
 
-        //if (Input.GetMouseButtonDown(0) == true || Input.GetMouseButtonDown(1) == true || Input.GetMouseButtonDown(2) == true)
-        //{
-        //    return;
-        //}
+        if (Input.GetButton("Submit"))
+        {
+            audioSource.PlayOneShot(sound01);
+        }
+
+        //左スティック（今までと同じ）
+        if (click != 0 && befor_button == 0.00f)
+        {
+            audioSource.PlayOneShot(sound02);
+        }
+
+        befor_button = click;
     }
 
     //クリックされた時にOnClickを呼び出すようにしておく
